d475e32a5d58fc98b83e6f612ec007101aab2146
Hanso <168148485+Hanso667@users.noreply.github.com>
2025-06-04 22:25:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="src/css/reset.css">
+    <link rel="stylesheet" href="src/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerador de Rifas</title>
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <main>
+        <form method="$_POST" class="Form">
+
+            <h1>Gerador de Rifas</h1>
+
+            <div class="Card-view">
+                <div class="Card">
+                    <label for="label" class="Label01">Titulo</label>
+                    <input type="text" name="Titulo" class="Input01">
+
+                    <label for="Descricao" class="Label01">Descrição</label>
+                    <textarea name="Descricao" class="Input01"></textarea>
+                </div>
+
+                <div class="Card">
+                    <label for="Valor" class="Label01">Valor</label>
+                    <input type="text" name="Valor" class="Input01">
+
+                    <label for="Quantidade" class="Label01">Quantidade</label>
+                    <input type="text" name="Quantidade" class="Input01">
+                </div>
+            </div>
+
+            <h2>Premios</h2>
+
+            <div class="Premios">
+
+                <div class="Card-Premio">
+                    <label for="Premio01" class="LabelPremios">Primeiro Premio</label>
+                    <input type="text" name="Premio01" class="InputPremios">
+                </div>
+
+                <div class="Card-Premio">
+                    <label for="Premio01" class="LabelPremios">Segundo Premio</label>
+                    <input type="text" name="Premio02" class="InputPremios">
+                </div>
+
+                <div class="Card-Premio">
+                    <label for="Premio01" class="LabelPremios">Terceiro Premio</label>
+                    <input type="text" name="Premio03" class="InputPremios">
+                </div>
+
+            </div>
+
+            <button type="submit" class="Button-submit">Enviar</button>
+
+        </form>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
